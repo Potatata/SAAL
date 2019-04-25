1a0c008e42b102573f91abf62187913c30c4c830
Ariel <ariel.ariasrojas@ucr.ac.cr>
2019-04-25 10:40:10 -0600

    Larger maps and tags were added to identify the trigger and walls

--- ExorcistSlime/Assets/Scripts/AnyManager.cs
+++ ExorcistSlime/Assets/Scripts/AnyManager.cs
@@ -8,7 +8,7 @@ public class AnyManager : MonoBehaviour
 
     bool gameStart;
     
-    //Al iniciar el juego se carga la primera escena
+    //Loads the  first scene when the game starts
     void Awake()
     {
         if(!gameStart)
@@ -16,17 +16,25 @@ public class AnyManager : MonoBehaviour
             anyManager = this;
 
             SceneManager.LoadSceneAsync(1, LoadSceneMode.Additive);
-
+            //Sets the player's initial position
+            GameObject player = GameObject.Find("Player");
+            if (player != null)
+            {
+                player.transform.position = new Vector2(21, 26);
+            }
             gameStart = true;
+
         }
         
     }
 
+    //Method to unload the scene
     public void UnloadScene(int scene)
     {
         StartCoroutine(Unload(scene));
     }
 
+    //Unload the scene
     IEnumerator Unload(int scene)
     {
         yield return null;
--- ExorcistSlime/Assets/Scripts/NextStage.cs
+++ ExorcistSlime/Assets/Scripts/NextStage.cs
@@ -8,15 +8,32 @@ public class NextStage : MonoBehaviour
 
     public int scene;
     bool loaded;
+    public int oldScene;
+    bool pastSceneUnloaded;
 
+    //Trigger to enter the next stage
     private void OnTriggerEnter2D()
     {
-        Debug.Log("Entro");
+        //Loads the next stage and unload the old scene
         if (!loaded)
         {
+            Debug.Log("Se cargó la escena");
+            GameObject player = GameObject.Find("Player");
             SceneManager.LoadSceneAsync(scene, LoadSceneMode.Additive);
+            //Sets the player position on the top of the level
+            if (player != null)
+            {
+                player.transform.position = new Vector2(21, 26);
+            }
             loaded = true;
         }
-        
+        if (!pastSceneUnloaded)
+        {
+            Debug.Log("Se eliminó la escena");
+            AnyManager.anyManager.UnloadScene(oldScene);
+            pastSceneUnloaded = true;
+        }
+
+
     }
 }
