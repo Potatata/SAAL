9ddfce88dfaf496d5794c80aa8a2087238454b5b
Luis Porras <luis.porrasledezma@ucr.ac.cr>
2019-04-28 17:31:07 -0600

    Add a very first try of making the dash trail

---
+++ ExorcistSlime/Assets/Scripts/SaltController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaltController : MonoBehaviour
+{
+    //Fields
+    public int lifeTime = 100;
+
+    void Update()
+    {
+        --lifeTime;
+        Debug.Log(lifeTime);
+        if (lifeTime < 0) Destroy(gameObject);
+    }
+}
