d8b564760d9e6c1466b1d5ad38a4054264ec2b27
Ariel Arias <ariel.ariasrojas@ucr.ac.cr>
2019-05-14 15:22:42 -0600

    Added MarkovChains

---
+++ ExorcistSlime/Assets/Scripts/MarkovChain/MarkovChain.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.MarkovChain
+{
+    public class MarkovChain
+    {
+        public int stages;
+        public double[,] markovProbabilities;
+
+        public int getNextState(int currentStateIndex)
+        {
+            Random randomNumber = new Random();
+
+            double randomValue = randomNumber.NextDouble();
+
+            double sum = 0;
+
+            for(int columnIterator = 0; columnIterator < stages; columnIterator++)
+            {
+                sum += markovProbabilities[currentStateIndex, columnIterator];
+                if (randomValue <= sum)
+                    return columnIterator;
+            }
+            return -1;
+        }
+    }
+}
