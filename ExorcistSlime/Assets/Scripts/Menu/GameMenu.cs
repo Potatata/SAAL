74e1aa8029a401e2bfb449a36d4932fd3ee5f96c
Sivana Alexa Hamer <sivanahamer@gmail.com>
2019-06-15 23:25:12 -0600

    All the sprite autogenerated files. Added a pause menu.

---
+++ ExorcistSlime/Assets/Scripts/Menu/GameMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMenu : MonoBehaviour
+{
+    private const int firstStage = 1;
+    /// <summary>
+    /// Starts the game
+    /// </summary>
+    public void ClickPlayButton()
+    {
+        SceneManager.LoadScene(firstStage);
+    }
+
+    /// <summary>
+    /// Quits the game
+    /// </summary>
+    public void ClickQuitButton()
+    {
+        Debug.Log("Hasta la vista");
+        Application.Quit();
+    }
+
+    /// <summary>
+    /// Restarts the game
+    /// </summary>
+    public void ClickPlayAgainButton()
+    {
+        PlayerInformation.GetInstance().RestartGame();
+        SceneManager.LoadScene(firstStage);
+    }
+}
---
+++ ExorcistSlime/Assets/Scripts/Menu/PauseMenuController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuController : MonoBehaviour
+{
+    public static bool isPaused = false;
+    public GameObject pauseMenuUI;
+
+    /// <summary>
+    /// Get the status if the game is currently paused or not
+    /// </summary>
+    /// <returns>The result if the game is paused or not</returns>
+    public bool getStatusOfPaused()
+    {
+        return isPaused;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+    /// <summary>
+    /// Resumes the game
+    /// </summary>
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+    /// <summary>
+    /// Pauses the game
+    /// </summary>
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    /// <summary>
+    /// Goes to main menu
+    /// </summary>
+    public void GoMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
